6f4f8dc6b7ab32733024d4891e7d56852cf0d92c
Luke Beatty <lukebeatty@Lukes-MacBook-Pro.local>
2019-04-13 11:02:05 -0400

    finished the about html and CSS and started working on the projects html and css

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
-(() => {
-let lucas = "Marco";
+console.log("fired!");
 
-console.log(`${lucas}), JavaScript is linked up!`);
+		var button 		= document.querySelector("#button");
+		var burgerCon	= document.querySelector(".burgerCon");
+		var hamEnd = document.querySelector("#hamEnd");
+		var mainHeader	= document.querySelector("#mainHeader");
+		var list = document.querySelector("li");
 
-})();
+		function hamburgerMenu() {
+			burgerCon.classList.toggle("slideToggle");
+			hamEnd.classList.toggle("slideToggle");
+			button.classList.toggle("slideToggle");
+			list.classList.toggle("slideToggle");
+
+		}
+
+		function closeMenu() {
+
+			if (window.matchMedia("(min-width: 1024px)").matches && burgerCon.classList.contains('slideToggle') ) {
+			console.log("media guery fired");
+			burgerCon.classList.remove('slideToggle');
+			button.classList.remove('expanded');
+			}
+		}
+
+		button.addEventListener("click", hamburgerMenu, false);
+
+		window.addEventListener("resize", closeMenu);
+
